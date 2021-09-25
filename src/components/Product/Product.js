dda221d91d02d5c4ec6bda327214ec7660d494a8
aaaasif <alexasif4@gmail.com>
2021-09-25 13:10:00 +0600

    js added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,11 +3,18 @@ import './Product.css'
 
 const Product = (props) => {
     console.log(props.product)
-    const {name, img, price} = props.product;
+    const {name, img, price, seller, stock} = props.product;
     return (
-        <div>
-            <img src={img} alt="" />
-            <h4>{name}</h4>
+        <div className ="Product">
+            <div>
+                 <img src={img} alt="" />
+            </div>
+            <div>
+                 <h4 className="product-name">{name}</h4>
+                 <p><small>by: {seller}</small></p>
+                 <p>Price: {price }</p>
+                 <p><small>only {stock} left in stock-order soon</small></p>
+            </div>
         </div>
     );
 };
