c11b7561cf813695b480b212d8c80282c007dda5
aaaasif <alexasif4@gmail.com>
2021-09-25 16:05:38 +0600

    js added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -15,7 +15,7 @@ const Product = (props) => {
                  <p>Price: {price}$</p>
                  <p><small>only {stock} left in stock-order soon</small></p>
                   <button 
-                      onClick={props.handleAddToCart}
+                      onClick={()=> props.handleAddToCart(props.product)}
                   className='btn-regular'
                   >Add to carf</button>
             </div>
