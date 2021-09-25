7bcc9b4bf9b52f436878fc4c9407c513ba7158f4
aaaasif <alexasif4@gmail.com>
2021-09-25 19:53:37 +0600

    js added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,11 +12,18 @@ const Cart = (props) => {
     // for(const product of cart) {
     //     total = total + product.price;
     // }
+
+    const shipping =15;
+    const tax = (total + shipping) * 10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
              <h3>order summary</h3>
             <h5>item order: {props.cart.length}</h5>
             <p>Total: {total}</p>
+            <p>shipping: {shipping}</p>
+            <p>tax: {tax}</p>
+            <p>Grand Total: {grandTotal}</p>
          </div>
     );
 };
