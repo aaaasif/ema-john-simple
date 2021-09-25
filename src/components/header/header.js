53b5ec5cf0111d922d6aae91a0021a50460bf27e
aaaasif <alexasif4@gmail.com>
2021-09-25 12:07:35 +0600

    js added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,6 +6,11 @@ const Header = () => {
     return (
         <div className="header">
             <img className="logo" src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Order</a>
+                <a href="/inventory">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
