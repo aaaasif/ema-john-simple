478f773cf397c62806b8ba21774557a9bc88b2cb
aaaasif <alexasif4@gmail.com>
2021-09-25 16:22:04 +0600

    js added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -30,7 +31,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                
+                <Cart></Cart>
             </div>
         </div>
     );
