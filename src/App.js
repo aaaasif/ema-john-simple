096d4777d432a296077b35dc56ec35248680f934
aaaasif <alexasif4@gmail.com>
2021-09-25 11:32:39 +0600

    js added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import header from './components/Header/Header';
+
 
 function App() {
   return (
     <div>
-      <git></git add .Header>
+      
     </div>
   );
 }
