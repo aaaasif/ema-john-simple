8f2167182cfc776eb56e00696c80445055680521
aaaasif <alexasif4@gmail.com>
2021-09-25 11:56:50 +0600

    js added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../../images/logo.png'
 
 const Header = () => {
     return (
         <div>
             <h1>hello from header</h1>
+            <img src={logo} alt="" />
         </div>
     );
 };
