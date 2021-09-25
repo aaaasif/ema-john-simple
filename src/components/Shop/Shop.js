c1f67e9572f60cb111d844c1c12e32e28d13b57c
aaaasif <alexasif4@gmail.com>
2021-09-25 12:43:21 +0600

    js added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 const Shop = () => {
+    const[products, setProducts] = useState([]);
+
+    useEffect( () => {
+        fetch('./products.JSON')
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }, [])
     return (
         <div className="shop-container">
             <div className="product-contaier">
