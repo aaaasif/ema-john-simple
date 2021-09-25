aa59b69df5f4b5e0c3a2f55b264f390cb4a15ed6
aaaasif <alexasif4@gmail.com>
2021-09-25 22:19:02 +0600

    js added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import {addToDb} from '../../utilities/fakedb'
 import './Shop.css'
 
 const Shop = () => {
@@ -15,6 +16,7 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb();
     }
 
     return (
