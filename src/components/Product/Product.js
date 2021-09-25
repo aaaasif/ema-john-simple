695d3da85a4406600ce481e401cfc116bd2afdf8
aaaasif <alexasif4@gmail.com>
2021-09-25 12:54:30 +0600

    js added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Product.css'
+
+const Product = () => {
+    return (
+        <div>
+            <h5>this is product</h5>
+        </div>
+    );
+};
+
+export default Product;
