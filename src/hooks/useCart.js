357629a0bfd46f36ac6227a7f3c6afbac08288e7
aaaasif <alexasif4@gmail.com>
2021-10-01 16:25:16 +0600

    js added

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import {getStoredCart} from '../utilities/fakedb'
+
 const useCart = products =>{
     const [cart, setCart] = useState([])
     useEffect( () =>{
