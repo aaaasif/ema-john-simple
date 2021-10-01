9092fbcc9ac879e64fb5013e392fa6d5fa28bb5e
aaaasif <alexasif4@gmail.com>
2021-10-01 11:48:21 +0600

    js added

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const OrderRevew = () => {
+    const [products] = useProducts();
     return (
         <div>
+            <h1>{products.length}</h1>
             <h2>kichui tw order korlen na vai</h2>
             <h1>kahini ki</h1>
         </div>
