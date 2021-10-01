3506e11b3f4761abed24f41c9749211cdbcf4da9
aaaasif <alexasif4@gmail.com>
2021-10-01 10:55:37 +0600

    js added

--- src/components/NotFound/NotFund.js
+++ src/components/NotFound/NotFund.js
@@ -3,6 +3,7 @@ import React from 'react';
 const NotFund = () => {
     return (
         <div>
+            <h1>404 eror</h1>
             <h1>tha page are you lokking for are not availavle</h1>
         </div>
     );
