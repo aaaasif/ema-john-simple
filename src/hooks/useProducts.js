734959930195affb8d39d218286c29f6a3ecdece
aaaasif <alexasif4@gmail.com>
2021-10-01 11:42:48 +0600

    js added

---
+++ src/hooks/useProducts.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react"
+
+const useProducts = () => {
+    const [products, setProducts ] = useState ([]);
+    useEffect (() => {
+        fetch('./products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, []);
+}
