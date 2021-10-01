81c711d3bd522e6294d31b254841499c3dbc31bb
aaaasif <alexasif4@gmail.com>
2021-10-01 20:46:56 +0600

    js added

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const ReviewItem = () => {
+const ReviewItem = (props) => {
+    const {name, price, quantity} = props.product;
     return (
-        <div>
-            
+        <div className='product'>
+            <div>
+                <h4 className='product-name'>{name}</h4>
+                <p>price: {price}</p>
+                <p>only {quantity} in stock. <Strong>Order Now</Strong></p>
+                <button className='btn-regular'>Remove</button>
+            </div>
         </div>
     );
 };
