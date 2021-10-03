543edc1aa6663bb451c7a181e63733ff79a8f4d4
aaaasif <alexasif4@gmail.com>
2021-10-03 11:25:54 +0600

    js added

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -3,7 +3,7 @@ import useProducts from '../../hooks/useProducts';
 import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router';
 
@@ -20,6 +20,8 @@ const OrderReview = () => {
 
     const handlePlaceOrder = () =>{
         history.push('/placeorder')
+        setCart([]);
+        clearTheCart();
     };
 
     return (
