8746b57e9389d61a3cda320e0f998e8d3ba72a35
aaaasif <alexasif4@gmail.com>
2021-10-03 11:59:47 +0600

    js added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -22,7 +22,19 @@ const Shop = () => {
     }, []);
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        const exists = cart.find(pd =>pd.key ===product.key);
+        let newCart =[];
+        if(exists) {
+            const rest = cart.filter(pd =>pd.key !==product.key);
+            exists.quantity = exists.quantity + 1;
+            newCart =[...rest, product]
+        }
+        else{
+            product.quantity =1;
+            newCart = [...cart, product];
+        }
+        // const newCart = [...cart, product];
+        
         setCart(newCart);
         // save to local storage (for now)
         addToDb(product.key);
