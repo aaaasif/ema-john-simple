00d0e73a82b2389999bcd0d4e6af543f184639e6
aaaasif <alexasif4@gmail.com>
2021-10-15 00:39:11 +0600

    js added

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+
+
+const initializeAuthentication = () => {
+    initializeApp(firebaseConfig);
+}
+
+export default initializeAuthentication;
+
+// step for authentication
---
+++ src/components/Login/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <div>
+                <h2>Login</h2>
+                <form onSubmit=''>
+                    <input type="email" name='' id='' placeholder="Yout Emai" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
