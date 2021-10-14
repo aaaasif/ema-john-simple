c611ae64815a1c64bdde4f657ff5e61cb9fefd42
aaaasif <alexasif4@gmail.com>
2021-10-15 00:02:26 +0600

    js added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwhJJwUexCrQ8myCGytelITPdwx0MDIUA",
+    authDomain: "ema-john-simple-f9791.firebaseapp.com",
+    projectId: "ema-john-simple-f9791",
+    storageBucket: "ema-john-simple-f9791.appspot.com",
+    messagingSenderId: "58807456501",
+    appId: "1:58807456501:web:87726d802ce9dc14e2abbc",
+    measurementId: "G-45ECW34347"
+  };
+
+  export default firebaseConfig;
