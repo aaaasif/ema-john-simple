9e4d6c741fa8637986cc5c658e911469eee4591b
aaaasif <alexasif4@gmail.com>
2021-10-16 11:42:36 +0600

    js added

---
+++ src/hooks/UseFirebase.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged , signOut} from "firebase/auth";
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    const signInUsingGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+        .then(result => {
+            console.log(result.user);
+        })
+    }
+    const logOut = () =>{
+        signOut(auth)
+            .then( () =>{
+
+            })
+    }
+    useEffect ( () => {
+        const auth = getAuth();
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            }
+        });
+    }, [])
+    return {
+        user,
+        signInUsingGoogle,
+        logOut
+    }
+}
+
+export default useFirebase;
