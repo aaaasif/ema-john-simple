6d8dd803f102bf2569338d3aaa0ce3ba2424815a
aaaasif <alexasif4@gmail.com>
2021-10-17 12:24:34 +0600

    js added

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import useFirebase from './../hooks/useFirebase';
+import useFirebase from '../hooks/useFirebase';
 
 export const AuthContext = createContext();
 
