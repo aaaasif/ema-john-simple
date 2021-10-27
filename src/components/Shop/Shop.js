184e1aa905dae549b39831b966b64f06bf716dc2
aaaasif <alexasif4@gmail.com>
2021-10-27 21:26:06 +0600

    pagination

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,6 +9,7 @@ import { Link } from 'react-router-dom';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useCart(products);
+    const [pageCount, setPageCount] = useState(0);
     // products to be rendered on the UI
     const [displayProducts, setDisplayProducts] = useState([]);
 
@@ -16,8 +17,11 @@ const Shop = () => {
         fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => {
-                setProducts(data);
-                setDisplayProducts(data);
+                setProducts(data.products);
+                setDisplayProducts(data.products);
+                const count = data.count;
+                const pageNumber = Math.ceil(count/10);
+                setPageCount(pageNumber);
             });
     }, []);
 
